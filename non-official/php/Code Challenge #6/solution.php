af4a1312e18636584e94a224dddffad11120b4e6
DonaldKellett <i.donaldl@me.com>
2016-05-28 19:37:55 +0800

    Refactor PHP solution and add more test cases

--- non-official/php/Code Challenge #6/solution.php
+++ non-official/php/Code Challenge #6/solution.php
@@ -1,13 +1,9 @@
 <?php
+list($gcd, $lcm) = array(function ($a, $b) {global $gcd; return $b === 0 ? $a : $gcd($b, $a % $b);}, function ($a, $b) {global $gcd; return $a * $b / $gcd($a, $b);});
 function convert_frac($fs) {
+  global $lcm;
   $ds = array_map(function ($f) {return $f[1];}, $fs);
-  $lcm = array_reduce($ds, function ($a, $b) {return lcm($a, $b);}, 1);
-  return implode(array_map(function ($f, $lcm) {return "(".$f[0]*$lcm/$f[1].",".$lcm.")";}, $fs,array_fill(0, count($fs), $lcm)));
-}
-function gcd($a, $b) {
-  return $b === 0 ? $a : gcd($b, $a % $b);
-}
-function lcm($a, $b) {
-  return $a * $b / gcd($a, $b);
+  $cd = array_reduce($ds, $lcm, 1);
+  return implode(array_map(function ($f, $cd) {return "(" . $f[0] * $cd / $f[1] . "," . $cd . ")";}, $fs, array_fill(0, count($fs), $cd)));
 }
 ?>
--- non-official/php/Code Challenge #6/tests.php
+++ non-official/php/Code Challenge #6/tests.php
@@ -8,7 +8,11 @@ $test->describe("convert_frac", function () {
     $test->assert_equals(convert_frac(array(array(2, 5), array(3, 8), array(5, 6), array(1, 4))), "(48,120)(45,120)(100,120)(30,120)");
     $test->assert_equals(convert_frac(array(array(2, 3), array(1, 3), array(1, 4), array(3, 4), array(7, 10), array(9, 10), array(1, 10))), "(40,60)(20,60)(15,60)(45,60)(42,60)(54,60)(6,60)");
     $test->assert_equals(convert_frac(array(array(1, 2), array(2, 3), array(1, 4), array(4, 5), array(5, 7), array(5, 6))), "(210,420)(280,420)(105,420)(336,420)(300,420)(350,420)");
-    $test->assert_equals(convert_frac(array(array(1, 3), array(2, 3), array(1, 21), array(2, 21), array(3, 7), array(4, 21), array(20, 21))), "(7,21)(14,21)(1,21)(2,21)(9,21)(4,21)(20,21)");
+    $test->assert_equals(convert_frac(array(array(1, 3), array(2, 3), array(1, 21), array(2, 21), array(3, 7), array(4, 21), array(20, 21), array(5, 7))), "(7,21)(14,21)(1,21)(2,21)(9,21)(4,21)(20,21)(15,21)");
+    $test->assert_equals(convert_frac(array(array(1, 2), array(3, 4), array(5, 8), array(5, 6), array(7, 12), array(9, 24))), "(12,24)(18,24)(15,24)(20,24)(14,24)(9,24)");
+    $test->assert_equals(convert_frac(array(array(1, 2), array(1, 4), array(3, 8), array(5, 12), array(5, 6))), "(12,24)(6,24)(9,24)(10,24)(20,24)");
+    $test->assert_equals(convert_frac(array(array(1, 2), array(3, 4), array(1, 6), array(3, 8), array(7, 10), array(1, 12))), "(60,120)(90,120)(20,120)(45,120)(84,120)(10,120)");
+    $test->assert_equals(convert_frac(array(array(1, 2), array(3, 4), array(5, 6), array(7, 8), array(9, 10), array(11, 12), array(13, 14), array(1, 14), array(3, 14))), "(420,840)(630,840)(700,840)(735,840)(756,840)(770,840)(780,840)(60,840)(180,840)");
   });
 });
 ?>
