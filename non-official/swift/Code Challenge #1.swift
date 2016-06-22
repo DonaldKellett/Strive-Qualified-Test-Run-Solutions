789eb9cf4bfe0cce747d51f5e2d94bb3a75d5228
DonaldKellett <i.donaldl@me.com>
2016-06-22 10:28:58 +0800

    Add first Swift code challenge

---
+++ non-official/swift/Code Challenge #1.swift
@@ -0,0 +1,24 @@
+import XCTest
+@testable import testing
+
+// Code
+var answer = 42, cake = "yummy"
+
+// Test Cases
+class testingCodeChallenge1: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testThatAnswerIs42() {
+        XCTAssert(answer == 42, "'answer' was not set to 42");
+    }
+    func testThatCakeIsYummy() {
+        XCTAssert(cake == "yummy", "'cake' was not set to 'yummy'");
+    }
+}
